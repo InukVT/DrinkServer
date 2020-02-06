f223164c96b45df07e0f26ced62f58c0ee55f236
Bastian Inuk Christensen <inuk@ruc.dk>
2020-02-06 22:03:10 +0100

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,8 +7,15 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    let hostname: String
+    #if os(Linux)
+    hostname = "psql"
+    #else
+    hostname = "localhost"
+    #endif
+    
     app.databases.use(.postgres(
-        hostname: Environment.get("POSTGRES_HOST") ?? "localhost",
+        hostname: Environment.get("POSTGRES_HOST") ?? hostname,
         username: Environment.get("POSTGRES_USER") ?? "vapor_username",
         password: Environment.get("POSTGRES_PASSWORD") ?? "vapor_password",
         database: Environment.get("POSTGRES_DB") ?? "vapor_database"
