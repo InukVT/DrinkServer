9a29a26fb2f3538b76e359235674677b7c544f7e
Bastian Inuk Christensen <bastian@inuk.blog>
2020-02-06 13:57:18 +0100

    Added VaporTester for testing class, to build up and tear down servers

---
+++ Tests/AppTests/VaporCase.swift
@@ -0,0 +1,36 @@
+import XCTVapor
+import App
+
+/// Conform to this, when writing test classes
+class VaporTester: XCTestCase {
+    
+    // MARK: Properties
+    
+    let app = Application(.testing)
+    
+    // MARK: Setup
+    
+    override func setUp() {
+        super.setUp()
+        
+        do {
+            try configure(app)
+            try app
+                .autoRevert()
+                .wait()
+            try app
+                .autoMigrate()
+                .wait()
+        }
+        catch {
+            XCTFail(#"Setup failed."#)
+        }
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        app.shutdown()
+    }
+
+}
