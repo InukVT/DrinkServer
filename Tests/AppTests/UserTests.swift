35cd2fefb43f7aff119d2267390c3c0c9832a7a1
Bastian Inuk Christensen <bastian@inuk.blog>
2020-02-06 22:34:03 +0100

    Removed user from first testcase

--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -9,6 +9,8 @@ final class UserTests: VaporTester {
         let fetchedUser = User.find(1, on: app.db) // Fetch user to get the
         fetchedUser.map { user -> Void in // Explicit void, because void can't be inferred
             XCTAssertTrue( user?.rights.contains(.canOrder) ?? false ) // If there's no user, then they can't order
+            
+            user?.delete(force: true, on: self.app.db) // Cleanly remove the user after use
         }
     }
 }
