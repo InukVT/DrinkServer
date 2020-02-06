d80543b8ee576d63ce2e5787f77c8ad869b0dbab
Bastian Inuk Christensen <bastian@inuk.blog>
2020-02-06 14:49:02 +0100

    Initial works on a user feature

---
+++ Sources/App/Models/UserModel.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bastian Inuk Christensen on 06/02/2020.
+//
+
+import Fluent
+
+final class User: Model {
+    
+    static var schema = "user"
+    
+    @ID(key: "id")
+    var id: Int?
+    
+    @Field(key: "mail")
+    var mail: String
+    
+    @Field(key: "password")
+    var password: String
+    
+    @Field(key: "rights")
+    var rights: UserRights
+    
+    // this is to conform to model
+    init() {}
+    
+    init(id: Int? = nil, mail: String, password: String, rights: UserRights = .canOrder) {
+        self.id = id
+    }
+}
+
+extension User {
+    /// This is like a bitmask, but much nicer handled
+    struct UserRights: OptionSet, Codable {
+        let rawValue: UInt8
+        
+        /// The user can order
+        public static let canOrder = UserRights(rawValue: 1 << 0)
+    }
+}
---
+++ Tests/AppTests/UserTests.swift
@@ -0,0 +1,14 @@
+@testable import App
+import XCTVapor
+
+final class UserTests: VaporTester {
+    func testUserRights() throws {
+        let testUser = User(mail: "inuk@ruc.dk", password: "hello")
+        testUser.save(on: app.db)
+        
+        let fetchedUser = User.find(1, on: app.db) // Fetch user to get the
+        fetchedUser.map { user -> Void in // Explicit void, because void can't be inferred
+            XCTAssertTrue( user?.rights.contains(.canOrder) ?? false ) // If there's no user, then they can't order
+        }
+    }
+}
